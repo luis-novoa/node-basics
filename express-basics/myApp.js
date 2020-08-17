2c85d277204f7b762ebc857ad18d5e4fb85dc017
Luis Novoa <luis.matte.novoa@gmail.com>
2020-08-17 10:04:35 -0400

    Adds endpoint to get time in json

--- express-basics/myApp.js
+++ express-basics/myApp.js
@@ -47,7 +47,12 @@ app.get('/json', function (req, res) {
 
 
 /** 8) Chaining middleware. A Time server */
-
+app.get('/now', (req, res, next) => {
+  req.time = new Date().toString();
+  next();
+}, (req, res) => {
+  res.json({ time: req.time });
+});
 
 /** 9)  Get input from client - Route parameters */
 
