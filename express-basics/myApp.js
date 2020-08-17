dc599d86c9de22a9b3fd13e1795ad52e439905c9
Luis Novoa <luis.matte.novoa@gmail.com>
2020-08-17 11:17:39 -0400

    Adds endpoint to handle post

--- express-basics/myApp.js
+++ express-basics/myApp.js
@@ -71,8 +71,9 @@ app.get('/name', (req, res) => {
 
 
 /** 12) Get data form POST  */
-
-
+app.post('/name', (req, res) => {
+  res.json({ name: `${req.body.first} ${req.body.last}` });
+});
 
 // This would be part of the basic setup of an Express app
 // but to allow FCC to run tests, the server is already active
