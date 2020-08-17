1bedc019f4972a92f1e28cba379c3fb06028af60
Luis Novoa <luis.matte.novoa@gmail.com>
2020-08-16 20:04:54 -0400

    Serves json

--- express-basics/myApp.js
+++ express-basics/myApp.js
@@ -26,7 +26,9 @@ const static_path = __dirname + '/public';
 app.use(express.static(static_path));
 
 /** 5) serve JSON on a specific route */
-
+app.get('/json', function (req, res) {
+  res.json({ "message": "Hello json" });
+})
 
 /** 6) Use the .env file to configure the app */
 
