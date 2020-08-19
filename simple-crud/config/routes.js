0ab5fd47df5ac239c887e2693543230e8672986f
Luis Novoa <luis.matte.novoa@gmail.com>
2020-08-18 23:08:27 -0400

    Adds routes.js

---
+++ simple-crud/config/routes.js
@@ -0,0 +1,4 @@
+const express = require('express');
+const routes = express.Router();
+
+modulo.exports = routes;
--- simple-crud/index.js
+++ simple-crud/index.js
@@ -1,8 +1,7 @@
 const express = require('express');
+const routes = require('./config/routes');
 const app = express();
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
+app.use(routes);
 
 app.listen(3000);
